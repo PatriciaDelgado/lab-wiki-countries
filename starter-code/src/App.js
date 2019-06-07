65ec0f85fc084b0da2401c15056b109596795151
Patricia Delgado <patricia.delgado.morcillo@gmail.com>
2019-06-07 20:03:38 +0200

    print sidebar with countries

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,19 +1,36 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom'
 import countries from './countries.json'
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import CountryDetails from './components/CountryDetail';
 
 
-class App extends Component {
+class App extends  Component {
+  state = {
+    countries: countries
+  }
   render() {
+    const list = countries.map((country, i) => 
+    <Link key={i} className="list-group-item list-group-item-action active">
+      {/* <span role="img" key={i}>{country.flag}</span> */}
+      {country.name.official}
+    </Link>)
+
     return (
       <div className="App">
         <nav className="navbar navbar-light bg-primary">
           <span>Wiki ountries</span>
         </nav>
         <div className="CountryList">
-
+          <div class="row">
+            <div class="col-5">
+              <div class="list-group">
+                {list}
+              </div>
+              </div>
+            <div class="col-7">Column 7/12</div>
+          </div>
         </div>
         <Switch>
           <Route exact path="/country/:id" component={CountryDetails} />
--- starter-code/src/components/CountryDetail.js
+++ starter-code/src/components/CountryDetail.js
@@ -1,9 +1,10 @@
 import React, { Component, Fragment } from 'react'
+import countries from '../countries.json'
 
 class CountryDetails extends Component {
   render() {
     return(
-      <div>este es un detalle</div>
+      <Fragment>este es un detalle</Fragment> 
     )
   }
 }
